aa2419c03e053e75a6020e3a6d938052941a7587
Marco Lizza <marco.lizza@gmail.com>
2019-01-08 17:58:22 +0100

    (WIP) Proper "use" path handling for the script engine.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <limits.h>
+#include <stdlib.h>
+
 #include <chaiscript/chaiscript.hpp>
 
 namespace raylib {
@@ -52,9 +55,35 @@ void chai_log(const std::string &message)
     raylib::TraceLog(raylib::LOG_DEBUG, message.c_str());
 }
 
+static void split_path(const std::string &path, std::string &parent, std::string &child)
+{
+#ifdef __linux__
+    const char PATH_SEPARATOR_CHAR = '/';
+    const char *PATH_SEPARATOR_STRING = "/";
+#elif _WIN32
+    const char PATH_SEPARATOR_CHAR = '\\';
+    const char *PATH_SEPARATOR_STRING = "\\";
+#endif
+    std::string::size_type i = path.rfind(PATH_SEPARATOR_CHAR);
+    if (i != std::string::npos) {
+        parent = path.substr(0, i);
+        child = path.substr(i + 1);
+    } else {
+        parent = "./";
+        child = path;
+    }
+
+    char *absolute_path = realpath(parent.c_str(), nullptr);
+    parent.assign(absolute_path);
+    parent.append(PATH_SEPARATOR_STRING); // ChaiScript need the paths to end with the separator!
+    free(absolute_path);
+
+    raylib::TraceLog(raylib::LOG_DEBUG, "path '%s' for file '%s'", parent.c_str(), child.c_str());
+}
+
 int main(int argc, char **argv)
 {
-    std::string script = "boot.chai";
+    std::string script = "./boot.chai";
     if (argc > 1) {
         script = argv[1];
     }
@@ -69,7 +98,14 @@ int main(int argc, char **argv)
 
     raylib::SetTargetFPS(60);
 
-    chaiscript::ChaiScript chai;
+    std::vector<std::string> modulepaths;
+    std::vector<std::string> usepaths;
+    std::string parent, child;
+    split_path(script, parent, child);
+    modulepaths.push_back(parent);
+    usepaths.push_back(parent);
+
+    chaiscript::ChaiScript chai(modulepaths, usepaths);
     chai.add(chaiscript::fun(&chai_log), "log");
     chai.eval_file(script);
 
