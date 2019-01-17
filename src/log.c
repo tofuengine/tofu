d2146fb6094fa36f8a09cae5e14b89cbfa71aeb2
Marco Lizza <marco.lizza@gmail.com>
2019-01-17 16:58:20 +0100

    Adding custom logging callback to avoid exit on error.

--- src/log.c
+++ src/log.c
@@ -7,8 +7,26 @@
 
 #define MAX_LOG_MESSAGE_LENGTH      256
 
+static void custom_log_callback(int msg_type, const char *text, va_list args)
+{
+    const char *prefix = "";
+    switch (msg_type) {
+        case LOG_INFO: prefix = "[INFO ] "; break;
+        case LOG_ERROR: prefix = "[ERROR] "; break;
+        case LOG_WARNING: prefix = "[WARN ] "; break;
+        case LOG_DEBUG: prefix = "[DEBUG] "; break;
+        case LOG_OTHER: prefix = "[OTHER] "; break;
+        default: break;
+    }
+
+    printf("%s", prefix);
+    vprintf(text, args);
+    printf("\n");
+}
+
 void Log_initialize()
 {
+    SetTraceLogCallback(custom_log_callback);
     SetTraceLog(LOG_DEBUG | LOG_INFO | LOG_WARNING | LOG_ERROR | LOG_OTHER);
 }
 
@@ -16,7 +34,7 @@ void Log_configure(bool enabled)
 {
     SetTraceLog(enabled
         ? LOG_DEBUG | LOG_INFO | LOG_WARNING | LOG_ERROR | LOG_OTHER
-        : LOG_WARNING | LOG_ERROR);
+        : LOG_ERROR);
 }
 
 void Log_write(Log_Levels_t level, const char *text, ...)
