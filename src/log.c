86023cc1e0a51a391a62cdee393bc4895ec57138
Marco Lizza <marco.lizza@gmail.com>
2019-01-21 10:34:03 +0100

    Log functions adapted to new API levels.

--- src/log.c
+++ src/log.c
@@ -11,11 +11,12 @@ static void custom_log_callback(int msg_type, const char *text, va_list args)
 {
     const char *prefix = "";
     switch (msg_type) {
-        case LOG_INFO: prefix = "[INFO ] "; break;
-        case LOG_ERROR: prefix = "[ERROR] "; break;
-        case LOG_WARNING: prefix = "[WARN ] "; break;
+        case LOG_TRACE: prefix = "[TRACE] "; break;
         case LOG_DEBUG: prefix = "[DEBUG] "; break;
-        case LOG_OTHER: prefix = "[OTHER] "; break;
+        case LOG_INFO: prefix = "[INFO ] "; break;
+        case LOG_WARNING: prefix = "[WARN ] "; break;
+        case LOG_ERROR: prefix = "[ERROR] "; break;
+        case LOG_FATAL: prefix = "[FATAL] "; break;
         default: break;
     }
 
@@ -27,14 +28,13 @@ static void custom_log_callback(int msg_type, const char *text, va_list args)
 void Log_initialize()
 {
     SetTraceLogCallback(custom_log_callback);
-    SetTraceLog(LOG_DEBUG | LOG_INFO | LOG_WARNING | LOG_ERROR | LOG_OTHER);
+    SetTraceLogLevel(LOG_ALL);
 }
 
 void Log_configure(bool enabled)
 {
-    SetTraceLog(enabled
-        ? LOG_DEBUG | LOG_INFO | LOG_WARNING | LOG_ERROR | LOG_OTHER
-        : LOG_ERROR);
+    SetTraceLogExit(LOG_NONE);
+    SetTraceLogLevel(enabled ? LOG_ALL : LOG_NONE);
 }
 
 void Log_write(Log_Levels_t level, const char *text, ...)
@@ -45,15 +45,5 @@ void Log_write(Log_Levels_t level, const char *text, ...)
     vsnprintf(message, MAX_LOG_MESSAGE_LENGTH, text, args);
     va_end(args);
 
-    int log_type;
-    switch (level) {
-        case LOG_LEVELS_TRACE: log_type = LOG_DEBUG; break;
-        case LOG_LEVELS_DEBUG: log_type = LOG_DEBUG; break;
-        case LOG_LEVELS_INFO: log_type = LOG_INFO; break;
-        case LOG_LEVELS_WARNING: log_type = LOG_WARNING; break;
-        case LOG_LEVELS_ERROR: log_type = LOG_ERROR; break;
-        case LOG_LEVELS_FATAL: log_type = LOG_ERROR; break;
-    }
-
-    TraceLog(log_type, message);
+    TraceLog((int)level, message);
 }
