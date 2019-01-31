3580989ae22245d6070ac3be34f6a775057c7d23
Marco Lizza <marco.lizza@gmail.com>
2019-01-31 16:19:20 +0100

    Better display initialization, supporting fullscreen.

--- src/display.c
+++ src/display.c
@@ -49,6 +49,17 @@ bool Display_initialize(Display_t *display, const Display_Configuration_t *confi
 
     SetExitKey(configuration->exit_key_enabled ? KEY_ESCAPE : -1);
 
+    // NOTE: this should be done as follows:
+    //
+    // if (configuration->fullscreen) {
+    //     SetWindowPosition(0, 0);
+    //     SetWindowSize(display_width, display_height);
+    // } else {
+    //     SetWindowPosition(x, y);
+    //     SetWindowSize(display->window_width, display->window_height);
+    // }
+    //
+    // However it would render the outer (unused) frame white. So, we limit the virtual window even when fullscreen.
     SetWindowPosition(x, y);
     SetWindowSize(display->window_width, display->window_height);
     UnhideWindow();
@@ -60,7 +71,11 @@ bool Display_initialize(Display_t *display, const Display_Configuration_t *confi
     SetTextureFilter(display->offscreen.texture, FILTER_POINT); // Nearest-neighbour scaling.
 
     display->offscreen_source = (Rectangle){ 0.0f, 0.0f, (float)display->offscreen.texture.width, (float)-display->offscreen.texture.height };
-    display->offscreen_destination = (Rectangle){ 0.0f, 0.0f, (float)display->window_width, (float)display->window_height };
+    if (configuration->fullscreen) {
+        display->offscreen_destination = (Rectangle){ (float)x, (float)y, (float)display->window_width, (float)display->window_height };
+    } else {
+        display->offscreen_destination = (Rectangle){ (float)0, (float)0, (float)display->window_width, (float)display->window_height };
+    }
     display->offscreen_origin = (Vector2){ 0.0f, 0.0f };
 
     return true;
@@ -83,20 +98,21 @@ void Display_renderBegin(Display_t *display, void callback(void))
 
 void Display_renderEnd(Display_t *display, void callback(void), const double fps, const double delta_time)
 {
-    EndTextureMode();
-    BeginDrawing();
-        if (callback) {
-            callback();
-        }
-        DrawTexturePro(display->offscreen.texture, display->offscreen_source, display->offscreen_destination,
-            display->offscreen_origin, 0.0f, WHITE);
-
         if (display->configuration.display_fps) {
             const char *text = FormatText("%.0f FPS (%.3fs)", fps, delta_time);
             int width = MeasureText(text, 10);
             DrawRectangle(0, 0, width, 10, (Color){ 0, 0, 0, 128 });
             DrawText(text, 0, 0, 10, (Color){ 255, 255, 255, 128 });
         }
+    EndTextureMode();
+
+    BeginDrawing();
+        // ClearBackground(BLACK);
+        if (callback) {
+            callback();
+        }
+        DrawTexturePro(display->offscreen.texture, display->offscreen_source, display->offscreen_destination,
+            display->offscreen_origin, 0.0f, WHITE);
     EndDrawing();
 }
 
