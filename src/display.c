782341ac9225b7de8e904b75392c9af551e884ba
Marco Lizza <marco.lizza@gmail.com>
2019-02-01 17:29:43 +0100

    Adding license headers.

--- src/display.c
+++ src/display.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2019 Marco Lizza (marco.lizza@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+
 #include "display.h"
 
 #include "log.h"
@@ -51,7 +73,7 @@ bool Display_initialize(Display_t *display, const Display_Configuration_t *confi
     SetExitKey(configuration->exit_key_enabled ? KEY_ESCAPE : -1);
 
 #ifdef FAST_FULLSCREEN
-    SetWindowPosition(x, y); // Enforce a "clipping region" if fullscreen.
+    SetWindowPosition(x, y); // This will enforce a "clipping region" for the fullscreen, clear won't be needed.
     SetWindowSize(display->window_width, display->window_height);
 #else
     if (configuration->fullscreen) {
