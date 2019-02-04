e9d4c50fa8b6536d96240a4ad7c50ce68c33c4e3
Marco Lizza <marco.lizza@gmail.com>
2019-02-04 17:51:19 +0100

    Fixing wrong macro usage.

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@
 int main(int argc, char **argv)
 {
     char resolved_path[PATH_FILE_MAX];
-    file_resolve_path(resolved_path, (argc > 1) ? argv[1] : FILE_PATH_CURRENT);
+    file_resolve_path(resolved_path, (argc > 1) ? argv[1] : FILE_PATH_CURRENT_SZ);
 
     Engine_t engine;
     bool result = Engine_initialize(&engine, resolved_path);
