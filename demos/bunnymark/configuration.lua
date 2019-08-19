02471e45cb9c988f16cd1c57c551c52a9d09cb3a
Marco Lizza <marco.lizza@gmail.com>
2019-08-19 17:18:59 +0200

    Purging unused configuration file.

--- demos/bunnymark/configuration.lua
+++
@@ -1,9 +0,0 @@
-return {
-    ["title"] = "BunnyMark",
-    ["width"] = 512,
-    ["height"] = 512,
-    ["fullscreen"] = false,
-    ["autofit"] = false,
-    ["exit-key-enabled"] = true,
-    ["debug"] = true
-}
