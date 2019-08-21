0c5df8465f7596becdf4e38c92e3d3950b6b6ae6
Marco Lizza <marco.lizza@gmail.com>
2019-08-21 18:23:00 +0200

    Porting `fire` demo to Lua.

---
+++ demos/fire/main.lua
@@ -0,0 +1,34 @@
+local Class = require("tofu.util.Class")
+
+local Main = Class.define()
+
+function Main:setup()
+  return {
+      ["title"] = "Fire",
+      ["width"] = 512,
+      ["height"] = 512,
+      ["fullscreen"] = false,
+      ["autofit"] = false,
+      ["exit-key-enabled"] = true,
+      ["debug"] = true
+  }
+end
+
+function Main:init()
+  local Game = require("game") -- Lazily require.
+  self.game = Game.new()
+end
+
+function Main:input()
+  self.game:input()
+end
+
+function Main:update(delta_time)
+  self.game:update(delta_time)
+end
+
+function Main:render(ratio)
+  self.game:render(ratio)
+end
+
+return Main
