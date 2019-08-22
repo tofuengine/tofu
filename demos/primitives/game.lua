dded411e2e719856119cedbd9df556c8ba14cbbb
Marco Lizza <marco.lizza@gmail.com>
2019-08-22 17:02:58 +0200

    Porting `primitives` demo to Lua.

---
+++ demos/primitives/game.lua
@@ -0,0 +1,43 @@
+local Canvas = require("tofu.graphics.Canvas")
+local Font = require("tofu.graphics.Font")
+local Environment = require("tofu.events.Environment")
+local Input = require("tofu.events.Input")
+local Class = require("tofu.util.Class")
+
+local Game = Class.define()
+
+function Game:__ctor()
+  Canvas.palette("arne-16")
+
+  self.font = Font.default()
+  self.segments = 30
+end
+
+function Game:input()
+  if Input.is_key_pressed(Input.START) then
+    Environment.quit()
+  elseif Input.is_key_pressed(Input.LEFT) then
+    self.segments = self.segments - 1
+  elseif Input.is_key_pressed(Input.RIGHT) then
+    self.segments = self.segments + 1
+  end
+end
+
+function Game:update(delta_time)
+end
+
+function Game:render(ratio)
+  Canvas.point(4, 4, 1)
+  Canvas.line(4, 8, 7, 8, 2)
+--Canvas.triangle("fill", 150, 150, 50, 250, 250, 250, 3)
+  Canvas.rectangle("fill", 4, 12, 8, 8, 3)
+  Canvas.rectangle("line", 4, 23, 8, 8, 3)
+--Canvas.square("fill", 200, 10, 75, 2)
+--Canvas.circle("line", 100, 100, 50, 2)
+--Canvas.circle("fill", 200, 100, 50, 1)
+--Canvas.circle("fill", 300, 100, 50, 1, _segments)
+
+  self.font:write(string.format("FPS: %d", Environment.fps()), Canvas.width(), 0, 1, 1.0, "right")
+end
+
+return Game
