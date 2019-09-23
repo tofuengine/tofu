ff51f708566589f6d4dd3a942bb8952f11f53abe
Marco Lizza <marco.lizza@gmail.com>
2019-09-23 10:43:42 +0200

    Fixing demo.

--- demos/palette/game.lua
+++ demos/palette/game.lua
@@ -33,7 +33,7 @@ function Game:input()
     self.palette = (self.palette % #PALETTES) + 1
     Canvas.palette(PALETTES[self.palette])
   elseif Input.is_key_pressed(Input.START) then
-    self.mode = (self.mode + 1) % 10
+    Canvas.screenshot("palette");
   elseif Input.is_key_pressed(Input.DOWN) then
     self.scale_y = 1.0
     self.y = self.y + 1
@@ -47,6 +47,8 @@ function Game:input()
     self.scale_x = -1.0
     self.x = self.x - 1
   elseif Input.is_key_pressed(Input.X) then
+    self.mode = (self.mode + 1) % 10
+  elseif Input.is_key_pressed(Input.Y) then
     self.clipping = not self.clipping
     if self.clipping then
       Canvas.clipping(32, 32, 95, 95)
@@ -65,6 +67,7 @@ function Game:update(delta_time)
 end
 
 function Game:render(ratio)
+  local time = System.time()
   Canvas.clear()
 
   if self.mode == 0 then
@@ -73,21 +76,21 @@ function Game:render(ratio)
       for j = 0, AMOUNT - 1 do
         local index = (i + j) % 7
         local color = (i + j) % AMOUNT
-        local y = (Canvas.height() - 8) * (math.sin(System.time() * 1.5 + i * 0.250 + j * 0.125) + 1) * 0.5
+        local y = (Canvas.height() - 8) * (math.sin(time * 1.5 + i * 0.250 + j * 0.125) + 1) * 0.5
         Canvas.shift(1, color)
         self.bank:blit(index, x, y)
       end
     end
   elseif self.mode == 1 then
-    local scale = (math.cos(System.time()) + 1) * 3
-    self.bank:blit(0, Canvas.width() / 2, Canvas.height() / 2, scale, scale, System.time() * 0.5, 0.5, 0.5)
+    local scale = (math.cos(time) + 1) * 3
+    self.bank:blit(0, Canvas.width() / 2, Canvas.height() / 2, scale, scale, time * 0.5, 0.5, 0.5)
   elseif self.mode == 2 then
     self.bank:blit(0, Canvas.width() / 2, Canvas.height() / 2, 10, 10, math.pi / 2 * 1, 0.5, 0.5)
   elseif self.mode == 3 then
     self.bank:blit(0, Canvas.width() / 2, Canvas.height() / 2, 10, 10, math.pi / 4 * 1, 0.5, 0.5)
   elseif self.mode == 4 then
-    local x = (Canvas.width() + 16) * (math.cos(self.time * 0.75) + 1) * 0.5 - 8
-    local y = (Canvas.height() + 16) * (math.sin(self.time * 0.25) + 1) * 0.5 - 8
+    local x = (Canvas.width() + 16) * (math.cos(time * 0.75) + 1) * 0.5 - 8
+    local y = (Canvas.height() + 16) * (math.sin(time * 0.25) + 1) * 0.5 - 8
     self.bank:blit(0, x - 4, y - 4)
   elseif self.mode == 5 then
     self.bank:blit(1, self.x - 32, self.y - 32, self.scale_x * 8.0, self.scale_y * 8.0)
