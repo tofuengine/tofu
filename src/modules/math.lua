8ea24a9ca9007175752f8ed3c9620bd212c627aa
Marco Lizza <marco.lizza@gmail.com>
2019-10-21 17:28:17 +0200

    Adding `Math` module.

--- src/modules/math.lua
+++ src/modules/math.lua
@@ -1,15 +1,28 @@
 local Math = {}
 
-function Math.tri(period, x)
-  return period - math.abs(x % (2 * period) - period)
+function Math.lerp(a, b, x)
+  -- More precise than `a + (b - a) * x`.
+  return a * (1.0 - x) + b * x
 end
 
-function Math.sincos(rotation)
-  return rotation, 1
+function Math.sine_wave(period, t)
+  local t_over_p = t / period
+  return math.sin(t_over_p * 2 * math.pi)
 end
 
-function Math.angle_to_rotation(angle)
-  return angle
+function Math.square_wave(period, t)
+  local t_over_p = t / period
+  return 2.0 * (2.0 * math.floor(t_over_p) - math.floor(2 * t_over_p)) + 1.0
+end
+
+function Math.triangle_wave(period, t)
+  local t_over_p = t / period
+  return 2.0 * math.abs(2.0 * (t_over_p - math.floor(t_over_p + 0.5))) - 1.0
+end
+
+function Math.sawtooth_wave(period, t)
+  local t_over_p = t / period
+  return 2.0 * (t_over_p - math.floor(0.5 + t_over_p))
 end
 
 return Math
