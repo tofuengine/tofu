49fa42f41e21b4d8e255f19d0596b2fb8d27492c
Marco Lizza <marco.lizza@gmail.com>
2019-10-29 14:21:19 +0100

    More complete snake demo.

--- demos/snake/game.lua
+++ demos/snake/game.lua
@@ -7,10 +7,44 @@ local System = require("tofu.core").System
 
 local CELLS = 32
 local LIFE = 1.0
-local SPEED = 5.0
+local SPEED = 10.0
 
 local DELTA_X = { up = 0, down = 0, left = -1, right = 1 }
 local DELTA_Y = { up = -1, down = 1, left = 0, right = 0 }
+
+local MAP =  "********************************"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "*                              *"
+          .. "********************************"
+
 
 local Game = Class.define()
 
@@ -33,52 +67,91 @@ function Game:__ctor()
   self.x_size = Canvas.width() / CELLS
   self.y_size = Canvas.height() / CELLS
   self.grid = Grid.new(CELLS, CELLS, 0)
-  self.length = 5
 dump(self)
 
   self:reset()
 end
 
+function Game:draw_map(map)
+  for i = 1, map:len() do
+    local c = map:byte(i)
+    local row = (i - 1) % CELLS
+    local column = (i - 1) / CELLS
+    local value = 0
+    if c == 42 then
+      value = -1
+    end
+    self.grid:poke(column, row, value)
+  end
+end
+
 function Game:reset()
-  self.position = { x = math.floor(CELLS / 2), y = math.floor(CELLS / 2) }
+  self.length = 5
+  self.state = "running"
+
+  self:draw_map(MAP)
+
+  self.position = { x = CELLS / 2, y = CELLS / 2 }
   self.accumulator = 0.0
-  self.grid:fill(0)
   for i = 1, self.length do
     self.grid:poke(self.position.x - self.length + i, self.position.y, i * LIFE)
   end
-  for row = 0, CELLS - 1 do
-    for column = 0, CELLS - 1 do
-      if row == 0 or column == 0 or row == CELLS -1 or column == CELLS - 1 then
-        self.grid:poke(column, row, -1)
-      end
-    end
-  end
   self.direction = "right"
+
+  self:generate_food()
 end
 
 function Game:input()
+  if self.state == "game-over" then
+    if Input.is_key_pressed(Input.START) then
+      self:reset()
+    end
+    return
+  end
+
+  if not self.can_move then
+    return
+  end
   if Input.is_key_pressed(Input.UP) then
     if self.direction ~= "down" then
       self.direction = "up"
+      self.can_move = false
     end
   elseif Input.is_key_pressed(Input.DOWN) then
     if self.direction ~= "up" then
       self.direction = "down"
+      self.can_move = false
     end
   elseif Input.is_key_pressed(Input.LEFT) then
     if self.direction ~= "right" then
       self.direction = "left"
+      self.can_move = false
     end
   elseif Input.is_key_pressed(Input.RIGHT) then
     if self.direction ~= "left" then
       self.direction = "right"
+      self.can_move = false
     end
-  elseif Input.is_key_pressed(Input.SELECT) then
-    self:reset()
+  end
+end
+
+function Game:generate_food()
+  while true do
+    local column = math.random(0, CELLS - 1)
+    local row = math.random(0, CELLS - 1)
+    local value = self.grid:peek(column, row)
+    if value == 0 then
+      self.grid:poke(column, row, -2)
+      break
+    end
   end
 end
 
 function Game:update(delta_time)
+  if self.state == "game-over" then
+    return
+  end
+
   self.grid:process(function(column, row, value)
       if value <= 0 then
         return column, row, value
@@ -89,11 +162,23 @@ function Game:update(delta_time)
 
   self.accumulator = self.accumulator + SPEED * delta_time
   while self.accumulator >= LIFE do
+    self.can_move = true
+
     self.accumulator = self.accumulator - LIFE
-    self.position.x = self.position.x + DELTA_X[self.direction]
-    self.position.y = self.position.y + DELTA_Y[self.direction]
-    -- collides(position)
+
+    self.position.x = (self.position.x + DELTA_X[self.direction]) % CELLS
+    self.position.y = (self.position.y + DELTA_Y[self.direction]) % CELLS
+
+    local value = self.grid:peek(self.position.x, self.position.y)
+
     self.grid:poke(self.position.x, self.position.y, self.length * LIFE)
+
+    if value == -2 then
+      self.length = self.length + 1
+      self:generate_food()
+    elseif value ~= 0 then
+      self.state = "game-over"
+    end
   end
 end
 
@@ -105,7 +190,9 @@ function Game:render(_)
   self.grid:scan(function(column, row, value)
       local x = column * w
       local y = row * h
-      if value == -1 then
+      if value == -2 then
+        Canvas.rectangle("fill", x, y, w, h, 3)
+      elseif value == -1 then
         Canvas.rectangle("fill", x, y, w, h, 1)
       elseif value == 0 then
         Canvas.rectangle("fill", x, y, w, h, 0)
