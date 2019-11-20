b000528977479cde2b80ecf701464cb2ee1a55ce
Marco Lizza <marco.lizza@gmail.com>
2019-11-20 16:02:39 +0100

    Adding info/warning/error/fatal loggers.

--- src/core/vm/modules/system.c
+++ src/core/vm/modules/system.c
@@ -35,13 +35,19 @@
 static int system_time(lua_State *L);
 static int system_fps(lua_State *L);
 static int system_quit(lua_State *L);
-static int system_log(lua_State *L);
+static int system_info(lua_State *L);
+static int system_warning(lua_State *L);
+static int system_error(lua_State *L);
+static int system_fatal(lua_State *L);
 
 static const struct luaL_Reg _system_functions[] = {
     { "time", system_time },
     { "fps", system_fps },
     { "quit", system_quit },
-    { "log", system_log },
+    { "info", system_info },
+    { "warning", system_warning },
+    { "error", system_error },
+    { "fatal", system_fatal },
     { NULL, NULL }
 };
 
@@ -91,11 +97,11 @@ static int system_quit(lua_State *L)
     return 0;
 }
 
-static int system_log(lua_State *L)
+static int log_write(lua_State *L, Log_Levels_t level)
 {
     int argc = lua_gettop(L);
     lua_getglobal(L, "tostring"); // F
-    for (int i = 1; i <= argc; i++) {
+    for (int i = 1; i <= argc; ++i) {
         lua_pushvalue(L, -1); // F -> F F
         lua_pushvalue(L, i); // F F -> F F I
         lua_call(L, 1, 1); // F F I -> F R
@@ -103,10 +109,30 @@ static int system_log(lua_State *L)
         if (s == NULL) {
             return luaL_error(L, "`tostring` must return a string `log`");
         }
-        Log_write(LOG_LEVELS_INFO, (i > 1) ? "\t%s" : "<SYSTEM> %s", s);
+        Log_write(level, (i > 1) ? "\t%s" : "<SYSTEM> %s", s);
         lua_pop(L, 1); // F R -> F
     }
     lua_pop(L, 1); // F -> <empty>
 
     return 0;
+}
+
+static int system_info(lua_State *L)
+{
+    return log_write(L, LOG_LEVELS_INFO);
+}
+
+static int system_warning(lua_State *L)
+{
+    return log_write(L, LOG_LEVELS_WARNING);
+}
+
+static int system_error(lua_State *L)
+{
+    return log_write(L, LOG_LEVELS_ERROR);
+}
+
+static int system_fatal(lua_State *L)
+{
+    return log_write(L, LOG_LEVELS_FATAL);
 }
