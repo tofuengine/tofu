4af4b7d573f20f24e84ba20e16da0e4f5989216b
Marco Lizza <marco.lizza@gmail.com>
2019-12-03 16:28:44 +0100

    Fixing timer demo.

--- demos/timers/configuration.lua
+++ demos/timers/configuration.lua
@@ -2,7 +2,7 @@ return {
   ["title"] = "Timers",
   ["width"] = 256,
   ["height"] = 256,
-  ["scale"] = 8,
+  ["scale"] = 2,
   ["fullscreen"] = false,
   ["exit-key-enabled"] = true,
   ["debug"] = true
