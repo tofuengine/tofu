1b52a5578abbaebcf18a55c889e9d30b1a092198
Marco Lizza <marco.lizza@gmail.com>
2019-12-13 15:48:02 +0100

    Using a packed structure to fetch RGBA componentes.

--- src/core/vm/modules/callbacks.c
+++ src/core/vm/modules/callbacks.c
@@ -24,15 +24,22 @@
 
 #include <memory.h>
 
+#pragma pack(push, 1)
+typedef struct rgba_t {
+    uint8_t r, g, b, a;
+} rgba_t;
+#pragma pack(pop)
+
 void surface_callback_palette(void *parameters, GL_Surface_t *surface, const void *data)
 {
     const GL_Palette_t *palette = (const GL_Palette_t *)parameters;
 
-    const uint8_t *src = (const uint8_t *)data; // rgba ordered.
+    const rgba_t *src = (const rgba_t *)data;
     GL_Pixel_t *dst = surface->data;
 
     for (size_t i = surface->data_size; i; --i) {
-        GL_Color_t color = (GL_Color_t){ .r = src[0], .g = src[1], .b = src[2], .a = src[3] }; src += 4;
+        rgba_t rgba = *(src++);
+        GL_Color_t color = (GL_Color_t){ .r = rgba.r, .g = rgba.g, .b = rgba.b, .a = rgba.a };
         *(dst++) = GL_palette_find_nearest_color(palette, color);
     }
 }
@@ -43,7 +50,7 @@ void surface_callback_indexes(void *parameters, GL_Surface_t *surface, const voi
     const GL_Pixel_t bg_index = indexes[0];
     const GL_Pixel_t fg_index = indexes[1];
 
-    const uint32_t *src = (const uint32_t *)data;
+    const uint32_t *src = (const uint32_t *)data; // Faster than the `rgba_t` struct, we don't need to unpack components.
     GL_Pixel_t *dst = surface->data;
 
     const uint32_t background = *src; // The top-left pixel color defines the background.
