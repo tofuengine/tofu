83b1777d94167b4a30e67123ffc9b45479b207da
Marco Lizza <marco.lizza@gmail.com>
2020-01-12 22:27:28 +0100

    Adding `hello-tofu`demo.

---
+++ demos/hello-tofu/main-0.lua
@@ -0,0 +1,58 @@
+--[[
+MIT License
+
+Copyright (c) 2019-2020 Marco Lizza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]--
+
+local Canvas = require("tofu.graphics").Canvas
+local Font = require("tofu.graphics").Font
+local Class = require("tofu.util").Class
+local System = require("tofu.core").System
+
+local Main = Class.define()
+
+local MESSAGE = "Hello, Tofu!"
+
+function Main:__ctor()
+  Canvas.palette("pico-8")
+
+  self.font = Font.default(0, 15)
+end
+
+function Main:input()
+end
+
+function Main:update(_)
+end
+
+function Main:render(_)
+  Canvas.clear()
+
+  local font_height = self.font:height()
+
+  local x = Canvas.width() * 0.5
+  local y = (Canvas.height() - font_height) * 0.5
+  self.font:write(MESSAGE, x, y, "center")
+
+  self.font:write(string.format("FPS: %d", System.fps()), 0, 0, "left")
+end
+
+return Main
