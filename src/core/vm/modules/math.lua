3a9b1e1e8b297252eb3029c5839b8d8c2faa3126
Marco Lizza <marco.lizza@gmail.com>
2020-02-21 23:41:46 +0100

    Annotating.

--- src/core/vm/modules/math.lua
+++ src/core/vm/modules/math.lua
@@ -24,27 +24,29 @@ SOFTWARE.
 
 local Math = {}
 
+-- TODO: move to C.
+
 function Math.lerp(a, b, r)
   -- More precise than `a + (b - a) * r`.
   return a * (1.0 - r) + b * r
 end
 
-function Math.sine_wave(period, t)
+function Math.sine_wave(period, t, min, max)
   local t_over_p = t / period
   return math.sin(t_over_p * 2 * math.pi)
 end
 
-function Math.square_wave(period, t)
+function Math.square_wave(period, t, min, max)
   local t_over_p = t / period
   return 2.0 * (2.0 * math.floor(t_over_p) - math.floor(2 * t_over_p)) + 1.0
 end
 
-function Math.triangle_wave(period, t)
+function Math.triangle_wave(period, t, min, max)
   local t_over_p = t / period
   return 2.0 * math.abs(2.0 * (t_over_p - math.floor(t_over_p + 0.5))) - 1.0
 end
 
-function Math.sawtooth_wave(period, t)
+function Math.sawtooth_wave(period, t, min, max)
   local t_over_p = t / period
   return 2.0 * (t_over_p - math.floor(0.5 + t_over_p))
 end
