7dbea9e0a11065c6cd1de8d72a7bc72ebbee04d6
Marco Lizza <marco.lizza@gmail.com>
2020-03-25 16:20:35 +0100

    Adding decoders.

--- src/core/io/audio.c
+++ src/core/io/audio.c
@@ -28,6 +28,12 @@
 #include <libs/log.h>
 
 #include <stdbool.h>
+#define DR_FLAC_IMPLEMENTATION
+#include <miniaudio/extras/dr_flac.h>    // Enables FLAC decoding.
+#define DR_MP3_IMPLEMENTATION
+#include <miniaudio/extras/dr_mp3.h>     // Enables MP3 decoding.
+#define DR_WAV_IMPLEMENTATION
+#include <miniaudio/extras/dr_wav.h>     // Enables WAV decoding.
 #define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio/miniaudio.h>
 
