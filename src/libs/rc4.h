afb8af2793ffd15defd84bab6ee1ed9c59fa3846
Marco Lizza <marco.lizza@gmail.com>
2020-09-30 00:35:55 +0200

    Minor touch-ups.

--- src/libs/rc4.h
+++ src/libs/rc4.h
@@ -1,5 +1,29 @@
-#ifndef __RC4_H__
-#define __RC4_H__
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2019-2020 Marco Lizza
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __LIBS_RC4_H__
+#define __LIBS_RC4_H__
 
 #include <stddef.h>
 #include <stdint.h>
@@ -12,4 +36,4 @@ typedef struct {
 extern void rc4_schedule(rc4_context_t *context, const uint8_t *key, size_t key_size);
 extern void rc4_process(rc4_context_t *context, uint8_t *data, size_t data_size);
 
-#endif  /* __RC4_H__ */
+#endif  /* __LIBS_RC4_H__ */
