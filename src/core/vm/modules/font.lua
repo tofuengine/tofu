810e588d6b47124ed9fe401b15373deba2d2ed36
Marco Lizza <marco.lizza@gmail.com>
2021-06-07 23:20:00 +0200

    Fixing style.

--- src/core/vm/modules/font.lua
+++ src/core/vm/modules/font.lua
@@ -28,16 +28,16 @@ local Font = {}
 -- Font.__index = Font
 
 local FONTS = {
-  ["5x8"] = { file = "5x8", width = 5, height = 8 },
-  ["6x12"] = { file = "6x12", width = 6, height = 12 },
-  ["8x16"] = { file = "8x16", width = 8, height = 16 },
-  ["12x24"] = { file = "12x24", width = 12, height = 24 },
-  ["16x32"] = { file = "16x32", width = 16, height = 32 },
-  ["32x64"] = { file = "32x64", width = 32, height = 64 },
-}
+    ["5x8"] = { file = "5x8", width = 5, height = 8 },
+    ["6x12"] = { file = "6x12", width = 6, height = 12 },
+    ["8x16"] = { file = "8x16", width = 8, height = 16 },
+    ["12x24"] = { file = "12x24", width = 12, height = 24 },
+    ["16x32"] = { file = "16x32", width = 16, height = 32 },
+    ["32x64"] = { file = "32x64", width = 32, height = 64 },
+  }
 
 function Font.default(...)
-  local Canvas = require("tofu.graphics").Canvas
+  local Canvas = require("tofu.graphics").Canvas -- Lazy `require()` to permit initial load.
 
   local args = { ... }
   if #args == 2 then -- background_color, foreground_color
@@ -57,6 +57,11 @@ end
 -- << [...] A function call that is not the last element in the list always produces one
 -- result [...] When a function call is the last (or the only) argument to another call,
 -- all results from the first call go as arguments. >>
+--
+-- The arguments are returned in the order required for the `Canvas.write()` method, that is
+--
+--   x, y, font, text, scale_x, scale_y
+--
 function Font:align(x, y, text, h_align, v_align, scale_x, scale_y)
   local width, height = self:size(text, scale_x or 1.0, scale_y or scale_x or 1.0)
 
@@ -76,8 +81,8 @@ function Font:align(x, y, text, h_align, v_align, scale_x, scale_y)
     dy = 0
   end
 
-  -- Return the proper amount of values in order to trigger the correct
-  -- `Font.write()` overloaded method.
+  -- Return the proper amount of values in order to trigger the correct `Canvas.write()` overloaded method.
+  -- Note that we *cannot* return `nil` for optional arguments or the wrong arity overload will be called.
   if scale_y then
     return x - dx, y - dy, self, text, scale_x, scale_y
   elseif scale_x then
