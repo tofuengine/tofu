4a2f786f3ce91d0150ff8d52bf97a4ef575bf544
Marco Lizza <marco.lizza@gmail.com>
2021-08-19 01:38:18 +0200

    Better mix-in support in the `Class` module.

--- src/core/vm/modules/class.lua
+++ src/core/vm/modules/class.lua
@@ -24,16 +24,8 @@ SOFTWARE.
 
 local Class = {}
 
-function Class.define(model)
+function Class.define()
   local proto = {}
-  -- If a base class is defined, the copy all the functions.
-  --
-  -- This is an instant snapshot, any new field defined runtime in the base
-  -- class won't be visible in the derived class.
-  if model then
-    Class.implement(proto, model)
-  end
-  -- This is the standard way in Lua to implement classes.
   proto.__index = proto
   proto.new = function(...)
       local self = setmetatable({}, proto)
@@ -45,10 +37,23 @@ function Class.define(model)
   return proto
 end
 
-function Class.implement(proto, model)
+function Class.borrow(proto, model, criteria)
   for key, value in pairs(model) do
-    if type(value) == "function" then
-      proto[key] = value
+    if type(value) == "function" and key ~= "new" then
+      local existing = proto[key]
+      if existing and criteria == "extend" then
+        proto[key] = function(...)
+            existing(...)
+            value(...)
+          end
+      elseif existing and criteria == "chain" then
+        proto[key] = function(...)
+            value(...)
+            existing(...)
+          end
+      elseif not existing or criteria == "replace" then
+        proto[key] = value
+      end
     end
   end
 end
