4bcf8cb48a0f1cb35062d011d0f4f21afd004624
Marco Lizza <marco.lizza@gmail.com>
2021-08-21 23:32:48 +0200

    Better demo.

--- demos/threedee/lib/camera.lua
+++ demos/threedee/lib/camera.lua
@@ -13,9 +13,11 @@ function Camera:__ctor(field_of_view, width, height, near, far)
 
   self.near = near
   self.far = far
-  self.depth = far - near
+  self.normalized_near = near / far
 
-  self.position = { 0.0, 0.0, 0.0 }
+  self.x = 0.0
+  self.y = 250.0
+  self.z = 0.0
 end
 
 function Camera:set_field_of_view(theta)
@@ -25,36 +27,32 @@ end
 function Camera:set_clipping_planes(near, far)
   self.near = near
   self.far = far
-  self.depth = far - near
+  self.normalized_near = near / far
 end
 
 function Camera:move(x, y, z)
-  self.position = { x, y, z }
+  self.x = x
+  self.y = y
+  self.z = z
 end
 
 -- function Camera:rotate(angle)
 -- end
 
-local function _is_visible(x, y, z)
-  return (x >= -1 and x <= 1) and (y >= -1 and y <= 1) and (z >= 0 and z <= 1)
-end
-
 function Camera:project(x, y, z)
-  local ox, oy, oz = table.unpack(self.position)
-
-  local dx = x - ox -- Transform to camera-space.
-  local dy = y - oy
-  local dz = z - oz
+  local cx = x - self.x -- Transform to camera-space.
+  local cy = y - self.y
+  local cz = z - self.z
 
   -- TODO: apply rotation here.
 
-  local d_over_dz = self.d / dz -- Transform to normalized projection plane
-  local cx = dx * d_over_dz / self.aspect_ratio
-  local cy = dy * d_over_dz
-  local cz = (dz - self.near) / self.depth
+  local d_over_dz = self.d / cz -- Transform to normalized projection plane
+  local px = cx * d_over_dz / self.aspect_ratio
+  local py = cy * d_over_dz
+  local pz = cz / self.far
 
-  if not _is_visible(cx, cy, cz) then
-    return { cx, cy, cz }, nil
+  if px < -1 or px > 1 or py < -1 or py > 1 or pz < self.normalized_near or pz > 1 then
+    return px, py, pz, nil, nil
   end
 
   -- Remap from `[-1, +1]` to `[0, width)` and `[0, height)`. The formula is
@@ -64,10 +62,10 @@ function Camera:project(x, y, z)
   --
   -- which can be simplified by pre-calculating the half width/height. Also note that the
   -- `y` component is mirrored as the display origin is at the top-left corner.
-  local sx = (1.0 + cx) * self.half_width - 1
-  local sy = (1.0 - cy) * self.half_height - 1
+  local sx = (1.0 + px) * self.half_width - 1
+  local sy = (1.0 - py) * self.half_height - 1
 
-  return { cx, cy, cz }, { sx, sy }
+  return px, py, pz, sx, sy
 end
 
 return Camera
--- demos/threedee/main.lua
+++ demos/threedee/main.lua
@@ -29,29 +29,27 @@ local Input = require("tofu.events").Input
 local Bank = require("tofu.graphics").Bank
 local Canvas = require("tofu.graphics").Canvas
 local Display = require("tofu.graphics").Display
-local Palette = require("tofu.graphics").Palette
 local Font = require("tofu.graphics").Font
+local Palette = require("tofu.graphics").Palette
+local Program = require("tofu.graphics").Program
+local Arrays = require("tofu.util").Arrays
 
 local Camera = require("lib.camera")
 
-local Main = Class.define()
+local COUNT <const> = 32
+local SPEED <const> = 150.0
 
-local points = {
-  {    0,  250, 1010 },
-  { -250, -250, 1010 },
-  {  250, -250, 1010 },
-  -- { -100, -100, 100 },
-  -- {  100, -100, 100 },
-  -- {  100,  100, 100 },
-  -- { -100,  100, 100 },
-  -- { -100, -100, 300 },
-  -- {  100, -100, 300 },
-  -- {  100,  100, 300 },
-  -- { -100,  100, 300 },
-}
+local Main = Class.define()
 
 function Main:__ctor()
   Display.palette(Palette.new("famicube"))
+
+  local program = Program.gradient(59, {
+    { 0, 0x98, 0xdc, 0xff },
+    { 96, 0x5b, 0xa8, 0xff },
+    { 128, 0x0a, 0x89, 0xff}
+  })
+  Display.program(program)
 
   local canvas = Canvas.default()
   local width, height = canvas:size()
@@ -61,11 +59,15 @@ function Main:__ctor()
   self.far = 1000
   self.field_of_view = math.pi / 2
 
-  self.bank = Bank.new(Canvas.new("assets/sheet.png", 63), 138, 138)
+  self.bank = Bank.new(Canvas.new("assets/sheet.png", 63), 40, 158)
   self.camera = Camera.new(self.field_of_view, width, height, self.near, self.far)
-  self.font = Font.default(0, 11)
+  self.font = Font.default(63, 11)
   self.easing = Math.tweener("quadratic_out")
   self.wave = Math.wave("triangle", 5.0, 500.0)
+
+  self.entities = {}
+
+  self.running = true
 end
 
 function Main:process()
@@ -89,6 +91,12 @@ function Main:process()
   elseif Input.is_pressed("x") then
     self.field_of_view = math.max(self.field_of_view - math.pi / 15, 0)
     update = true
+  elseif Input.is_pressed("b") then
+    self.camera.y = math.min(self.camera.y + 5.0, 500.0)
+  elseif Input.is_pressed("a") then
+    self.camera.y = math.max(self.camera.y - 5.0, 0)
+  elseif Input.is_pressed("start") then
+    self.running = not self.running
   end
 
   if update then
@@ -97,27 +105,78 @@ function Main:process()
   end
 end
 
-function Main:update(_)
+local function _fartherst_first(a, b)
+  return a.z < b.z
+end
+
+local function reverse_ipairs(table)
+  return function(a, i)
+        i = i - 1
+        if i == 0 then
+          return nil, nil
+        end
+        return i, a[i]
+    end, table, #table + 1
+end
+
+function Main:update(delta_time)
+  if not self.running then
+    return
+  end
+
+  if #self.entities < COUNT then
+    table.insert(self.entities, { x = math.random(-1000, 1000), y = 0, z = 1000.0 }) -- Spawn them on ground level.
+  end
+
+  for _, entity in ipairs(self.entities) do
+    entity.z = entity.z - SPEED * delta_time
+  end
+
+  Arrays.sort(self.entities, _fartherst_first)
+end
+
+local function _distance(a, b)
+  local dx = a.x - b.x
+  local dy = a.y - b.y
+  local dz = a.z - b.z
+  local d = math.sqrt(dx * dx + dy * dy + dz * dz)
+  return math.max(math.min(d / 1500.0, 1.0), 0.0)
 end
 
 function Main:render(_)
   local canvas = Canvas.default()
-  canvas:clear()
-
-  local t = 0 --System.time()
+  local width, _ = canvas:size()
+  canvas:clear(59)
 
   local camera = self.camera
-  for _, point in ipairs(points) do
-    local x, y, z = table.unpack(point)
-    z = z - (self.wave(t) + 500)
-    local c, s = camera:project(x, y, z)
-    if s then
-      local cx, cy, cz = table.unpack(c)
-      local sx, sy = table.unpack(s)
-      local sz = 1.0 - self.easing(cz)
-      local scale = sz * 8
-      self.bank:blit(canvas, sx, sy, 0, scale, scale, 0)
---      canvas:square('fill', dx, dy, scale, 32)
+
+  local i = 0
+  for z = camera.far, camera.near, -50 do
+    local _, _, _, _, sy = camera:project(camera.x, 0.0, z) -- Straight forward the camera, on ground level.
+    local _, _, _, _, syp1 = camera:project(camera.x, 0.0, z - 50)
+    if not sy or not syp1 then
+      break
+    end
+    --print(z .. " " .. sy .. " " .. syp1)
+    canvas:rectangle('fill', 0, sy, width, syp1 - sy + 1, i % 2 == 0 and 18 or 22)
+    i = i + 1
+  end
+
+  for index, entity in reverse_ipairs(self.entities) do
+    local _, _, pz, sx, sy = camera:project(entity.x, entity.y, entity.z)
+    if not sx or not sy then
+      --print(string.format("dead at %.3f, %.3f, %.3f", px, py, pz))
+      --print(index .. " " .. #self.entities)
+      table.remove(self.entities, index)
+    else
+      local d = pz or _distance(camera, entity)
+      local sz = 1.0 - self.easing(d) -- Scaling factor should depend on camera-to-entity distance!
+      local scale = sz * 4
+      local w, h = self.bank:size(0, scale, scale)
+      self.bank:blit(canvas, sx - w * 0.5, sy - h * 1.0, 0, scale, scale) -- Align the bottom center.
+      --canvas:square('fill', sx - 2, sy - 2, 4, 17)
+--      self.font:write(canvas, sx, sy,
+--        string.format("%.3f %.3f %.3f (%.3f)", px, py, pz, d), "center", "middle")
 --      self.font:write(canvas, dx, dy, string.format("%.3f %.3f %.3f", x, y, z), "center", "middle")
     end
   end
