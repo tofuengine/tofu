bc17fe585b640663a07db893fa699919004d41e7
Marco Lizza <marco.lizza@gmail.com>
2025-11-03 02:21:39 +0100

    Adding support for folder operations, output filename is implicit.

--- extras/tools/imagen.lua
+++ extras/tools/imagen.lua
@@ -41,9 +41,25 @@ SOFTWARE.
 --  1 lua-vips
 --  2 argparse
 
+-- The image format use is the following:
+--   [0-7]     : (8 bytes) Signature `TOFUIMG!`
+--   [8-9]     : (2 bytes) Width (little-endian)
+--   [10-11]   : (2 bytes) Height (little-endian)
+--   [12-13]   : (2 bytes) Palette length, up to 256 entries (little-endian)
+--   [14-...]  : (palette length * 3 bytes) Palette data (RGB triplets)
+--   [...-end] : (width * height bytes) Pixel data (each byte is an 8-bit index in the palette)
+
 local argparse = require("argparse")
+local lfs = require("lfs")
 local vips = require("vips")
 
+-- -----------------------------------------------------------------------------
+-- HELPER FUNCTIONS ------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+
+-- Initialize the logging functions based on the current command-line flags.
+-- By default, logging is enabled. If the `quiet` flag is set, logging is
+-- disabled. If the `verbose` flag is set, verbose logging is enabled.
 local function log_init(args)
   if args.quiet then
     log = function(...)
@@ -56,7 +72,7 @@ local function log_init(args)
     log = function(...)
       print(string.format(...))
     end
-    if args.detailed then
+    if args.verbose then
       log_debug = function(...)
         print(string.format(...))
       end
@@ -69,7 +85,7 @@ local function log_init(args)
 end
 
 local function load_image(path)
-  log("Loading image %s...", path)
+  log("Loading image `%s`", path)
 
   local image = vips.Image.new_from_file(path, { access = "random" })
   if image:bands() == 3 then
@@ -116,12 +132,15 @@ local function from_rgba32(rgb)
     return r, g, b
 end
 
+-- -----------------------------------------------------------------------------
+-- COLOR QUANTIZATION ----------------------------------------------------------
+-- -----------------------------------------------------------------------------
+
 -- Iterate over each pixel in the image and compute the histogram of colors.
 -- The histogram is a table mapping from color (as 32-bit RGB integer) to
 -- occurrence count.
 local function compute_histogram(image)
   local histogram = {}
-
   for_each_pixel(image,
     function(x, y, r, g, b, a)
         local rgb = to_rgba32(r, g, b)
@@ -240,8 +259,8 @@ local function split_bucket(bucket)
   --
   -- pivot = first index i such that cumulative[i] >= total/2
   for index, color in ipairs(bucket) do
-    log_debug("  color %08x with count %d",
-      to_rgba32(color.r, color.g, color.b), color.count)
+    log_debug("  color %02x%02x%02x with count %d",
+      color.r, color.g, color.b, color.count)
   end
 
   log_debug("Finding pivot point...")
@@ -250,8 +269,8 @@ local function split_bucket(bucket)
   local half_count = info.count / 2 -- Float division, we are OK with that.
   for index, color in ipairs(bucket) do
     cumulative_count = cumulative_count + color.count
-    log_debug("  color %08x with count %d brings running count to %d",
-      to_rgba32(color.r, color.g, color.b), color.count, cumulative_count)
+    log_debug("  color %02x%02x%02x with count %d brings running count to %d",
+      color.r, color.g, color.b, color.count, cumulative_count)
     if cumulative_count >= half_count then
       pivot_index = index
       log_debug("  -> pivot found at index %d", pivot_index)
@@ -272,20 +291,20 @@ local function split_bucket(bucket)
     log_debug("Dominant color detected at pivot")
     for index, color in ipairs(bucket) do
       if index == pivot_index then
-        log_debug("  -> color %08x goes to bucket A", to_rgba32(color.r, color.g, color.b))
+        log_debug("  -> color %02x%02x%02x goes to bucket A", color.r, color.g, color.b)
         table.insert(bucket_a, color)
       else
-        log_debug("  -> color %08x goes to bucket B", to_rgba32(color.r, color.g, color.b))
+        log_debug("  -> color %02x%02x%02x goes to bucket B", color.r, color.g, color.b)
         table.insert(bucket_b, color)
       end
     end
   else
     for index, color in ipairs(bucket) do
       if index <= pivot_index then
-        log_debug("  -> color %08x goes to bucket A", to_rgba32(color.r, color.g, color.b))
+        log_debug("  -> color %02x%02x%02x goes to bucket A", color.r, color.g, color.b)
         table.insert(bucket_a, color)
       else
-        log_debug("  -> color %08x goes to bucket B", to_rgba32(color.r, color.g, color.b))
+        log_debug("  -> color %02x%02x%02x goes to bucket B", color.r, color.g, color.b)
         table.insert(bucket_b, color)
       end
     end
@@ -294,6 +313,8 @@ local function split_bucket(bucket)
   return bucket_a, bucket_b
 end
 
+-- Compute the average color of a bucket (which contains multiple colors), as
+-- the weighter mean.
 local function average_bucket_color(bucket)
   local r_sum, g_sum, b_sum = 0, 0, 0
   local count = 0
@@ -311,9 +332,11 @@ local function average_bucket_color(bucket)
   local b = math.tointeger(math.floor(b_sum / count + 0.5))
   log_debug("Average color: R=%d, G=%d, B=%d", r, g, b)
 
-  return to_rgba32(r, g, b)
+  return { r = r, g = g, b = b }
 end
 
+-- Calculate a palette of the given size from the image using the median-cut
+-- algorithm.
 local function calculate_palette(image, colors)
   local bucket = compute_histogram(image)
   if not bucket then
@@ -322,6 +345,7 @@ local function calculate_palette(image, colors)
   local buckets = { bucket }
   log_debug("Initial bucket with %d colors", #bucket)
 
+  -- Keep splitting until we reach the desired number of colors on no more splittable buckets.
   while #buckets < colors do
     local index = pick_best_bucket(buckets)
     if not index then
@@ -343,42 +367,24 @@ local function calculate_palette(image, colors)
   for _, bucket in ipairs(buckets) do
     log_debug("Final bucket with %d colors", #bucket)
 
-    local rgb = average_bucket_color(bucket)
-    table.insert(palette, rgb)
-    log_debug("new palette entry added: %08x", rgb)
+    local color = average_bucket_color(bucket)
+    table.insert(palette, color)
+    log_debug("new palette entry added: %02x%02x%02x", color.r, color.g, color.b)
   end
-
-  table.sort(palette)
 
   return palette
 end
 
-local function emit_header(writer, image, palette)
-  local width, height = image:width(), image:height()
-
-  -- `TOFUIMG!` (8 bytes, ASCII)
-  -- width (2 bytes, little-endian)
-  -- height (2 bytes, little-endian)
-  -- palette length (2 bytes, little-endian)
-  -- palette data (palette size * 3 bytes)
-  writer:write(string.pack("c8", "TOFUIMG!"))
-  writer:write(string.pack("<I2", width))
-  writer:write(string.pack("<I2", height))
-  writer:write(string.pack("<I2", #palette))
-  for _, rgb in ipairs(palette) do
-    local r, g, b = from_rgba32(rgb)
-    writer:write(string.pack("BBB", r, g, b))
-  end
-end
-
+-- Find the best matching color in the palette for the given RGB color.
+-- The best matching color is the one with the smallest Euclidean distance
+-- in the RGB color space.
 local function match_color_in_palette(r, g, b, palette)
   local best_index = 0
   local best_distance = math.huge
-  for index, crgb in ipairs(palette) do
-    local cr, cg, cb = from_rgba32(crgb)
-    local dr = r - cr
-    local dg = g - cg
-    local db = b - cb
+  for index, color in ipairs(palette) do
+    local dr = r - color.r
+    local dg = g - color.g
+    local db = b - color.b
     local distance = (dr * dr) + (dg * dg) + (db * db)
     if distance < best_distance then
       best_distance = distance
@@ -388,7 +394,24 @@ local function match_color_in_palette(r, g, b, palette)
   return best_index
 end
 
--- pixel data (width * height bytes, each byte is an index in the palette)
+-- -----------------------------------------------------------------------------
+-- I/O FUNCTIONS ---------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+
+local function emit_header(writer, header)
+  writer:write(string.pack("c8", "TOFUIMG!"))
+  writer:write(string.pack("<I2", header.width))
+  writer:write(string.pack("<I2", header.height))
+  writer:write(string.pack("<I2", #header.palette))
+  for _, color in ipairs(header.palette) do
+    writer:write(string.pack("BBB", color.r, color.g, color.b))
+  end
+  for index = #header.palette + 1, 256 do
+    local pixel = index - 1
+    writer:write(string.pack("BBB", pixel, pixel, pixel)) -- Pad the palette to 256 entries.
+  end
+end
+
 local function emit_data(writer, image, palette)
   local cache = {} -- Memoization cache for color lookups.
 
@@ -413,7 +436,13 @@ local function write_image(path, image, palette)
     return false
   end
 
-  emit_header(writer, image, palette)
+  local header = {
+    width = image:width(),
+    height = image:height(),
+    palette = palette
+  }
+
+  emit_header(writer, header)
   emit_data(writer, image, palette)
 
   writer:close()
@@ -421,8 +450,10 @@ local function write_image(path, image, palette)
   return true
 end
 
--- Load and parse a palette file in the simple hexadecimal format used by
--- https://lospec.com/palette-list
+-- Load and parse a palette file in the simple hexadecimal format where each
+-- line contains a RGB hex triplet (e.g. `ff00ff` for magenta).
+--
+-- See: https://lospec.com/palette-list
 local function load_and_parse_palette(path)
   local reader = io.open(path, "rb")
   if not reader then
@@ -430,7 +461,7 @@ local function load_and_parse_palette(path)
     return nil
   end
 
-  local palette = { }
+  local palette = {}
   while true do
     local line = reader:read("*l")
     if not line then
@@ -444,8 +475,9 @@ local function load_and_parse_palette(path)
       return nil
     end
 
-    log("new palette entry found: %08x", rgb)
-    table.insert(palette, rgb)
+    local r, g, b = from_rgba32(rgb)
+    log_debug("new palette entry found: %02x%02x%02x", r, g, b)
+    table.insert(palette, { r = r, g = g, b = b })
   end
 
   reader:close()
@@ -453,34 +485,112 @@ local function load_and_parse_palette(path)
   return palette
 end
 
-local function convert_command(args)
-  local image = load_image(args.input)
+local function read_header(reader)
+  local signature, width, height, palette_size = string.unpack("c8<I2<I2<I2", reader:read(14))
 
-  log(string.format("Processing image %s as %s", args.input, args.output))
+  if signature ~= "TOFUIMG!" then
+    log("*** invalid image signature: expected `TOFUIMG!`, got `%s`", signature)
+    return nil
+  end
 
-  local palette = args.palette
-    and load_and_parse_palette(args.palette)
-    or calculate_palette(image, args.colors or 256)
+  local palette = {}
+  for i = 1, palette_size do
+    local r, g, b = string.unpack("BBB", reader:read(3))
+    table.insert(palette, { r = r, g = g, b = b })
+  end
+
+  return {
+    width = width,
+    height = height,
+    palette = palette
+  }
+end
+
+-- -----------------------------------------------------------------------------
+-- COMMANDS --------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+
+local function convert_command(input, palette, colors, sort)
+  local image = load_image(input)
+
+  log("Converting image %s", input)
+
+  local palette = #palette > 0
+    and load_and_parse_palette(palette)
+    or calculate_palette(image, colors or 256)
 
   if not palette then
-    log("*** failed to calculate palette")
+    log("*** failed to obtain the palette")
     return false
   end
 
-  for index, rgb in ipairs(palette) do
-    log("palette[%3d] = %08x", index - 1, rgb)
+  if sort then
+    log("sorting palette...")
+    table.sort(palette, function(a, b)
+        return a.r < b.r or
+                (a.r == b.r and a.g < b.g) or
+                (a.r == b.r and a.g == b.g and a.b < b.b)
+      end)
   end
 
-  local success = write_image(args.output, image, palette)
+  log("Palette size: %d colors", #palette)
+  for index, color in ipairs(palette) do
+    log("  [%3d] = %02x%02x%02x", index - 1, color.r, color.g, color.b)
+  end
+
+  -- The output file name is the same of the input, but with `.img` extension.
+  local output = input:gsub("%.%w+$", "") .. ".img"
+  log("Writing output image `%s`", output)
+  local success = write_image(output, image, palette)
 
   log(success and "Done!" or "Failed!")
 
   return success
 end
 
-local function command_extract(args)
-  log("*** extract command not implemented yet")
-  return false
+-- Reads and displays the header of an image file. The size and the palette
+-- colors are printed. This is useful to check the actual dimensions of an
+-- image and the palette bound to it.
+local function command_inspect(input)
+  log("Inspecting image `%s`", input)
+
+  local reader = io.open(input, "rb")
+  if not reader then
+    log("*** can't open file `%s`", input)
+    return false
+  end
+
+  local header = read_header(reader, image)
+  if not header then
+    log("*** failed to read image header")
+    return false
+  end
+
+  log("Image size: %dx%d", header.width, header.height)
+  log("Palette size: %d colors", #header.palette)
+  for index, color in ipairs(header.palette) do
+    log("  [%3d] = %02x%02x%02x", index - 1, color.r, color.g, color.b)
+  end
+
+  reader:close()
+
+  return true
+end
+
+local function is_directory(path)
+  return path:sub(-1) == "/" or lfs.attributes(path, "mode") == "directory"
+end
+
+local function expand_path(path, filter, paths)
+  if is_directory(path) then
+    for entry in lfs.dir(path) do
+      if entry ~= "." and entry ~= ".." then
+        expand_path(path .. "/" .. entry, filter, paths)
+      end
+    end
+  elseif path:match(filter) then
+    table.insert(paths, path)
+  end
 end
 
 local function main(arg)
@@ -489,32 +599,29 @@ local function main(arg)
     :name("imagen")
     :description("Image generator.")
   parser:argument("command")
-    :choices({'convert', 'extract'})
-    :description("Command to be executed (only `convert` is supported).")
+    :choices({'convert', 'inspect'})
+    :description("Command to be executed.")
     :args(1)
   parser:argument("input")
     :description("Path of the input image to be converted.")
     :args(1)
-  parser:option("-o --output")
-    :description("Name of the the generated image.")
-    :default("aout.img")
-    :args('?')
-    :count(1)
   parser:option("-c --colors")
     :description("Sets the size of the palette.")
     :default("256")
-    :args('?')
-    :convert(math.tointeger)
+    :args(1)
     :count(1)
+    :convert(math.tointeger)
   parser:option("-p --palette")
     :description("Path of the palette file.")
     :default("")
-    :args('?')
+    :args(1)
     :count(1)
+  parser:flag("-s --sort")
+    :description("Sort the palette entries before converting the image.")
   parser:flag("-q --quiet")
-    :description("Enables quiet output during image conversion.")
-  parser:flag("-d --detailed")
-    :description("Enables detailed output during image conversion.")
+    :description("Suppress any output during image conversion.")
+  parser:flag("-v --verbose")
+    :description("Enables verbose output during image conversion.")
   local args = parser:parse(arg)
 
   --for k, v in pairs(args) do
@@ -523,12 +630,22 @@ local function main(arg)
 
   log_init(args)
 
-  log("ImageN v0.2.0")
+  log("ImageN v0.3.0")
   log("=============")
 
+  local paths = {}
+  expand_path(args.input, "%.png$", paths)
+
   local success = false
-  if args.command == "convert" then
-    success = convert_command(args)
+  for _, path in ipairs(paths) do
+    if args.command == "convert" then
+      success = convert_command(path, args.palette, args.colors, args.sort)
+    elseif args.command == "inspect" then
+      success = command_inspect(path)
+    end
+    if not success then
+      break
+    end
   end
 
   os.exit(not success and -1 or 0)
