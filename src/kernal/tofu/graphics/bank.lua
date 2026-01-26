a178b8a23cf8bc59194b1c77b7aecdc40561b31e
Marco Lizza <marco.lizza@gmail.com>
2026-01-26 23:43:59 +0100

    feat(api): adding `Bank.from_file()` method

---
+++ src/kernal/tofu/graphics/bank.lua
@@ -0,0 +1,55 @@
+--[[
+                ___________________  _______________ ___
+                \__    ___/\_____  \ \_   _____/    |   \
+                  |    |    /   |   \ |    __) |    |   /
+                  |    |   /    |    \|     \  |    |  /
+                  |____|   \_______  /\___  /  |______/
+                                   \/     \/
+        ___________ _______    ________.___ _______  ___________
+        \_   _____/ \      \  /  _____/|   |\      \ \_   _____/
+         |    __)_  /   |   \/   \  ___|   |/   |   \ |    __)_
+         |        \/    |    \    \_\  \   /    |    \|        \
+        /_______  /\____|__  /\______  /___\____|__  /_______  /
+                \/         \/        \/            \/        \
+
+MIT License
+
+Copyright (c) 2019-2026 Marco Lizza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]--
+
+local Image <const> = require("tofu.graphics.image")
+
+local Bank <const> = {}
+
+function Bank.from_image(...)
+  local args <const> = { ... }
+  if #args == 1 then -- name
+    return Bank.new(Image.new(args[1]))
+  elseif #args == 2 then -- name, cells_file
+    return Bank.new(Image.new(args[1]), args[2])
+  elseif #args == 3 then -- name, cell_width, cell_height
+    return Bank.new(Image.new(args[1]), args[2], args[3])
+  else
+    error("invalid arguments for `from_image` method", 2)
+  end
+end
+
+return Bank
