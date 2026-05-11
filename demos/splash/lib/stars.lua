bb7f3812c782426fdd6a9072307e449c5a5d88b2
Marco Lizza <marco.lizza@gmail.com>
2026-05-11 23:47:23 +0200

    chore(demo): reworking splash demo, better stars scattering

--- demos/splash/lib/stars.lua
+++ demos/splash/lib/stars.lua
@@ -35,31 +35,36 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ]]--
 
-local Class = require("tofu.core.class")
-local Math = require("tofu.core.math")
-local Bank = require("tofu.graphics.bank")
-local Arrays = require("tofu.util.arrays")
+local Class <const> = require("tofu.core.class")
+local Math <const> = require("tofu.core.math")
+local Bank <const> = require("tofu.graphics.bank")
+local Arrays <const> = require("tofu.util.arrays")
 
-local STAR_WIDTH = 23
-local STAR_HEIGHT = 23
+local STAR_WIDTH <const> = 23
+local STAR_HEIGHT <const> = 23
 
-local MAX_STARS_COUNT = 32
+local STARS_SPAWN_INTERVAL <const> = 0.5
+local MAX_STARS_COUNT <const> = 32
 
-local Stars = Class.define()
+local START_ROTATION_SPEED_FACTOR <const> = 5.0
+
+local Stars <const> = Class.define()
 
 function Stars:__ctor(width, height, _, pool)
-  local area = { x0 = 0, y0 = 0, x1 = width - 1, y1 = height - 1 }
+  local area <const> = { x0 = 0, y0 = 0, x1 = width - 1, y1 = height - 1 }
 
   self.pool = {}
   self.bank = Bank.from_image("assets/images/atlas.img", STAR_WIDTH, STAR_HEIGHT)
-  self.timer = pool:spawn(0.5, 0, function(_)
+  self.timer = pool:spawn(STARS_SPAWN_INTERVAL, 0, function(_)
       if #self.pool >= MAX_STARS_COUNT then
         return
       end
 
-      local scale = math.random(1, 16) * 0.5
-      local vx = math.random(1, 5) * 7 * (math.random(0, 1) > 0.5 and -1 or 1)
-      local vy = math.random(3, 5) * 11
+      local depth <const> = math.random(1, 10) -- scale and speed depends of depth
+
+      local scale <const> = depth * 0.5
+      local vx <const> = scale * (7 + math.random(3, 5)) * (math.random(0, 1) > 0.5 and -1 or 1)
+      local vy <const> = scale * (11 + math.random(3, 9))
 
       local w, h = STAR_WIDTH * scale * 0.5, STAR_HEIGHT * scale * 0.5
 
@@ -68,7 +73,7 @@ function Stars:__ctor(width, height, _, pool)
           scale = scale,
           area = { x0 = area.x0 - w, y0 = area.y0 - h, x1 = area.x1 + w, y1 = area.y1 + h },
           vx = vx, vy = vy,
-          vr = vx * 7.0, -- Rotate in the "direction" of the Y movement, faster with speed.
+          vr = vx * START_ROTATION_SPEED_FACTOR, -- Rotate in the "direction" of the Y movement, faster with speed.
           x = math.random(area.x0 - w, area.x1 + w),
           y = area.y0 - h,
           rotation = math.random(0, Math.SINCOS_PERIOD)
