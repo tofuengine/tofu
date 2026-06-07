ad87891e760cb8eb1321e470a55d46e5cc11ab6c
Marco Lizza <marco.lizza@gmail.com>
2026-06-07 22:54:29 +0200

    feat(libs): improving fixed-point library

--- src/libs/fpmath.h
+++ src/libs/fpmath.h
@@ -38,18 +38,137 @@
 #ifndef TOFU_LIBS_FPMATH_H
 #define TOFU_LIBS_FPMATH_H
 
+#define FPMATH_H_INCLUDED
+
 #include <stdint.h>
 
-#define FIXED32_SHIFT 16
-#define FIXED32_ONE   (1 << FIXED32_SHIFT)
+/*
+ * The library refers to the actual type as `fixed32_t` as internally it is
+ * just an integer.
+ * If not specified otherwise, the fixed-point numbers are represented as 16.16
+ * (i.e. 16 bits for the integer part and 16 bits for the fractional part).
+ */
 
+#if !defined(FIXED32_FRACTIONAL_BITS)
+    #define FIXED32_FRACTIONAL_BITS 16
+    #define FIXED32_FRACTIONAL_MASK 0x0000FFFF
+    #define FIXED32_INTEGER_MASK 0xFFFF0000
+#endif  /* !defined(FIXED32_FRACTIONAL_BITS) */
+
+#define FIXED32_ONE (1 << FIXED32_FRACTIONAL_BITS)
+#define FIXED32_ONE_HALF (FIXED32_ONE >> 1)
+
+//typedef int32_t fp32_16_t;
 typedef int32_t fixed32_t;
 
-#define FIXED32_FROM_FLOAT(v) ((fixed32_t)((v) * (float)FIXED32_ONE + ((v) >= 0 ? 0.5f : -0.5f)))
-#define FIXED32_FROM_DOUBLE(v) ((fixed32_t)((v) * (double)FIXED32_ONE + ((v) >= 0 ? 0.5 : -0.5)))
+// All the functions are defined as `static inline` to allow the compiler to
+// optimize them as much as possible, and to avoid potential issues with
+// multiple definitions when included in different translation units.
+static inline fixed32_t fixed32_from_int(int v)
+{
+    return (fixed32_t)(v << FIXED32_FRACTIONAL_BITS);
+}
 
-#define FIXED32_TO_FLOAT(v) ((float)(v) / (float)FIXED32_ONE)
-#define FIXED32_TO_DOUBLE(v) ((double)(v) / (double)FIXED32_ONE)
-#define FIXED32_TO_INT(v) ((int)((v) >> FIXED32_SHIFT))
+static inline fixed32_t fixed32_from_float(float v)
+{
+    return (fixed32_t)(v * (float)FIXED32_ONE);
+}
+
+static inline fixed32_t fixed32_from_float_round(float v)
+{
+    return (fixed32_t)(v * (float)FIXED32_ONE + (v >= 0 ? 0.5f : -0.5f));
+}
+
+static inline fixed32_t fixed32_from_double(double v)
+{
+    return (fixed32_t)(v * (double)FIXED32_ONE);
+}
+
+static inline fixed32_t fixed32_from_double_round(double v)
+{
+    return (fixed32_t)(v * (double)FIXED32_ONE + (v >= 0 ? 0.5 : -0.5));
+}
+
+static inline int fixed32_to_int(fixed32_t v)
+{
+    return v >> FIXED32_FRACTIONAL_BITS;
+}
+
+static inline int fixed32_to_int_round(fixed32_t v)
+{
+    if (v >= 0) {
+        return (v + FIXED32_ONE_HALF) >> FIXED32_FRACTIONAL_BITS;
+    }
+    return (v - FIXED32_ONE_HALF) >> FIXED32_FRACTIONAL_BITS;
+}
+
+static inline float fixed32_to_float(fixed32_t v)
+{
+    return (float)v / (float)FIXED32_ONE;
+}
+
+static inline float fixed32_to_float_round(fixed32_t v)
+{
+    if (v >= 0) {
+        return (float)(v + FIXED32_ONE_HALF) / (float)FIXED32_ONE;
+    }
+    return (float)(v - FIXED32_ONE_HALF) / (float)FIXED32_ONE;
+}
+
+static inline double fixed32_to_double(fixed32_t v)
+{
+    return (double)v / (double)FIXED32_ONE;
+}
+
+static inline double fixed32_to_double_round(fixed32_t v)
+{
+    if (v >= 0) {
+        return (double)(v + FIXED32_ONE_HALF) / (double)FIXED32_ONE;
+    }
+    return (double)(v - FIXED32_ONE_HALF) / (double)FIXED32_ONE;
+}
+
+static inline fixed32_t fixed32_floor(fixed32_t v)
+{
+    return v & FIXED32_INTEGER_MASK;
+}
+
+static inline fixed32_t fixed32_ceil(fixed32_t v)
+{
+    if (v >= 0) {
+        return (v & FIXED32_INTEGER_MASK) + ((v & FIXED32_FRACTIONAL_MASK) ? FIXED32_ONE : 0);
+    }
+    return (v & FIXED32_INTEGER_MASK) - ((v & FIXED32_FRACTIONAL_MASK) ? FIXED32_ONE : 0);
+}
+
+static inline fixed32_t fixed32_round(fixed32_t v)
+{
+    if (v >= 0) {
+        return (v + FIXED32_ONE_HALF) & FIXED32_INTEGER_MASK;
+    }
+    return (v - FIXED32_ONE_HALF) & FIXED32_INTEGER_MASK;
+}
+
+// Helper macros. The user can still call the functions directly if they want
+// to, but these macros provide a more convenient interface. Also, they allow
+// to switch beetween rounding and non-rounding versions of the functions by
+// defining the `FIXED32_NO_ROUNDING` macro.
+#define FIXED32_FROM_INT(v) fixed32_from_int(v)
+#if !defined(FIXED32_NO_ROUNDING)
+    #define FIXED32_FROM_FLOAT(v) fixed32_from_float_round(v)
+    #define FIXED32_FROM_DOUBLE(v) fixed32_from_double_round(v)
+    #define FIXED32_TO_INT(v) fixed32_to_int_round(v)
+    #define FIXED32_TO_FLOAT(v) fixed32_to_float_round(v)
+    #define FIXED32_TO_DOUBLE(v) fixed32_to_double_round(v)
+#else   /* !defined(FIXED32_NO_ROUNDING) */
+    #define FIXED32_FROM_FLOAT(v) fixed32_from_float(v)
+    #define FIXED32_FROM_DOUBLE(v) fixed32_from_double(v)
+    #define FIXED32_TO_INT(v) fixed32_to_int(v)
+    #define FIXED32_TO_FLOAT(v) fixed32_to_float(v)
+    #define FIXED32_TO_DOUBLE(v) fixed32_to_double(v)
+#endif  /* !defined(FIXED32_NO_ROUNDING) */
+
+#define FIXED32_ITRUNC(v) fixed32_to_int(v)
+#define FIXED32_IROUND(v) fixed32_to_int_round(v)
 
 #endif  /* TOFU_LIBS_FPMATH_H */
