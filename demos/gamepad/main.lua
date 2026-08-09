09db501efc22f384a843d0f72659f0553790350c
Marco Lizza <marco.lizza@gmail.com>
2026-08-09 15:47:25 +0200

    fix(demo): updating `gamepad` demo

--- demos/gamepad/main.lua
+++ demos/gamepad/main.lua
@@ -48,8 +48,6 @@ local Cursor = require("tofu.input.cursor")
 local PALETTE <const> = Palette.default("pico-8")
 local PALETTE_FONT <const> = Palette.new({{ 0, 255, 0 }})
 local FONT <const> = Font.default()
-local CANVAS <const> = Canvas.default()
-local WIDTH <const>, HEIGHT <const> = CANVAS:image():size()
 local CONTROLLER <const> = Controller.default()
 local CURSOR <const> = Cursor.default()
 
@@ -69,7 +67,10 @@ local INDICES <const> = {
 
 local Main = Class.define()
 
-function Main:__ctor()
+function Main:__ctor(canvas)
+  self.canvas = canvas
+  self.width, self.height = canvas:image():size()
+
   self.bank = Bank.from_image("assets/sheet.img", 12, 12)
   self.down = {}
   self.scale = {}
@@ -120,14 +121,16 @@ local function draw_trigger(canvas, cx, cy, radius, magnitude)
   canvas:circle("line", cx, cy, radius, 1)
 end
 
-function Main:render(canvas, _)
+function Main:render(_)
+  local canvas <const> = self.canvas
+
   canvas:clear(0)
 
   local t = System.time()
 
   local cw, ch = self.bank:size(Bank.NIL)
 
-  local x, y = (WIDTH - #IDS * cw) * 0.5, (HEIGHT - ch) * 0.5
+  local x, y = (self.width - #IDS * cw) * 0.5, (self.height - ch) * 0.5
   for index, id in ipairs(IDS) do
     local dy = math.sin(t * 2.5 + x * 0.5) * ch
     if self.down[id] then
@@ -144,7 +147,7 @@ function Main:render(canvas, _)
     x = x + cw
   end
 
-  local cy = HEIGHT * 0.5
+  local cy = self.height * 0.5
   local lx, ly, la, lm = CONTROLLER:stick("left")
   local rx, ry, ra, rm = CONTROLLER:stick("right")
   draw_stick(canvas, 24, cy - 12, 8, lx, ly, la, lm, CONTROLLER:is_down("lt"))
@@ -165,7 +168,7 @@ function Main:render(canvas, _)
   canvas:push()
     canvas:bank(1)
     canvas:write(0, 0, FONT, string.format("%d FPS", System.fps()))
-    canvas:write(WIDTH, HEIGHT, FONT, string.format("X:%.2f Y:%.2f A:%.2f M:%.2f", lx, ly, la, lm),
+    canvas:write(self.width, self.height, FONT, string.format("X:%.2f Y:%.2f A:%.2f M:%.2f", lx, ly, la, lm),
       "right", "bottom")
   canvas:pop()
 end
